56a272d472d254e3a827938e740e001a77f3bee1
Zenith <z@zenith.dev>
2021-03-17 09:30:45 -0700

    Refactor for new aurflux version

--- statbot.py
+++ statbot.py
@@ -1,28 +1,27 @@
 from __future__ import annotations
 
-import asyncio
+import typing as ty
 
 import TOKENS
 import aurcore
 import aurflux
-from aurflux.argh import *
 import stats
+import discord
+
 if ty.TYPE_CHECKING:
     from aurflux.command import *
 
 
-class Statbot():
+class Statbot:
     def __init__(self):
-        self.event_router = aurcore.event.EventRouter(name=self.__class__.__name__)
-        self.flux = aurflux.Aurflux(self.__class__.__name__, admin_id=TOKENS.ADMIN_ID, parent_router=self.event_router)
-
+        self.event_router = aurcore.event.EventRouterHost(name=self.__class__.__name__)
+        self.flux = aurflux.FluxClient(self.__class__.__name__, admin_id=TOKENS.ADMIN_ID, parent_router=self.event_router, intents=discord.Intents.all())
 
     async def startup(self, token: str):
         await self.flux.startup(token)
 
     async def shutdown(self):
         await self.flux.logout()
-
 
 
 roombot = Statbot()
